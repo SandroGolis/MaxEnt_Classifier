c2a479d5a013ceff9169c66c5e668aff70c98c48
Sandro Golis <golis.sandro@gmail.com>
2016-10-17 09:39:01 -0400

    Mini Batch gradient descent

--- src/key_maxent.py
+++ src/key_maxent.py
@@ -1,10 +1,15 @@
 # -*- mode: Python; coding: utf-8 -*-
+from __future__ import division
+from random import seed, shuffle
 
 from classifier import Classifier
 import numpy as np
+import scipy.misc as scp
 
-LEARNING_RATE = 0.0001
-BATCH_SIZE = 30
+LEARNING_RATE = 0.0001  # learning rate for SGD
+BATCH_SIZE = 50  # mini batch size
+MAX_EPOCHS = 400  # max number of epochs
+STEP_SIZE = 5  # checking convergence every STEP_SIZE iterations
 BIAS_FEATURE = '_demo_feature_for_bias_'
 
 
@@ -32,7 +37,7 @@ class MaxEntModel(object):
 
             For example: if map_name_to_id[feature1] = f_id_1 then
                          w1_A is a weight corresponding to feature
-               f_1A(X, C) = 1 if C=classA AND feature1 is in features(X)
+               f_1A(X, C) = 1 if C=class_A AND feature1 is in features(X)
                             0 otherwise
             4. observed count     type: numpy.array(rows, columns)
                matrix with the same shape as weights_matrix.
@@ -48,21 +53,28 @@ class MaxEntModel(object):
         self.weights_matrix = np.zeros((self.rows, self.cols))
         self.observed_count = None
 
-    def update_weights(self, gradient, learning_rate):
-        self.weights_matrix += learning_rate * gradient
+    def update_weights(self, weights):
+        self.weights_matrix = np.copy(weights)
+
+
+def generate_batches(data, batch_size):
+    """
+        This generator yields slices of data.
+        Each slice is a view on the initial data and it's size equals to batch_size
+        if data doesn't divides evenly by mini batches, the last slice will be shorter
+    """
+    start_index = 0
+    while True:
+        last_index = start_index + batch_size
+        yield data[start_index: last_index]
+        start_index = last_index
+        if start_index > data.size:
+            return
 
 
 class MaxEnt(Classifier):
     def __init__(self, new_model=None):
         super(MaxEnt, self).__init__(new_model)
-
-    def get_model(self):
-        return self._model
-
-    def set_model(self, new_model):
-        self._model = new_model
-
-    model = property(get_model, set_model)
 
     def train(self, instances, dev_instances=None):
         """
@@ -70,6 +82,8 @@ class MaxEnt(Classifier):
                 I will build 2 data structures
                 1. model.map_name_to_id (type: dictionary)
                    mapping: feature_name ---> feature_id
+                   I will add a feature: BIAS_FEATURE to each data instance
+                   in order to represent bias term in the weights matrix.
                 2. model.map_label_to_col (type: dictionary)
                    mapping: class_name ---> class_column
 
@@ -82,16 +96,23 @@ class MaxEnt(Classifier):
         map_name_to_id = {BIAS_FEATURE: 0}
         labels = {}
         for instance in instances:
-            fnames = instance.features()
+
+            # store mapping of each unseen label to a new column number
             label = instance.label
             if label not in labels:
                 labels[label] = len(labels)
 
+            # store mapping of each unseen feature to a new feature id
+            # todo should remove stop words somewhere.
+            # If here - means no feature can be named like one of the stopwords. Bad constraint.
+            # Removing inside document.features() function bad because user needs to remove.
+            # But maybe good for us for this assignment
+            fnames = instance.features()
             for fname in fnames:
                 if fname not in map_name_to_id:
                     map_name_to_id[fname] = len(map_name_to_id)
 
-            # Cache the features sparse vector internally within each document
+            # Cache features sparse vector representation within each document
             fnames.append(BIAS_FEATURE)
             instance.feature_vector = self.get_sparse_vector(fnames, map_name_to_id)
 
@@ -99,45 +120,79 @@ class MaxEnt(Classifier):
         self.model = MaxEntModel(map_name_to_id, labels)
 
         # Calculate weights using Mini-batch Gradient Descent
-        self.train_sgd(instances, dev_instances, LEARNING_RATE, BATCH_SIZE)
+        self.train_sgd(instances, dev_instances)
 
-    def train_sgd(self, train_instances, dev_instances, learning_rate, batch_size):
+    def train_sgd(self, train_instances, dev_instances):
         """
             Train MaxEnt model with Mini-batch Gradient Descent.
+            Using global parameters LEARNING_RATE and BATCH_SIZE
         """
-        # Calculate empirical count of features in the given training set.
-        self.model.observed_count = self.calc_empirical_count(train_instances)
+        iteration = 0
+        last_accuracy = 0
+        weights = self.model.weights_matrix
 
-        # todo check convergence? define maximum iterations?
-        # todo mini-batch
-        for i in range(0, 10):
-            # Calculate expected count with current model
-            expected_count = self.calc_expected_count(train_instances)
-            # calculate gradient
-            gradient = self.model.observed_count - expected_count
-            # update weights
-            self.model.update_weights(gradient, learning_rate)
+        for epoch in xrange(MAX_EPOCHS):
+            seed(hash("shuffle_for_epoch" + str(epoch)))
+            shuffle(train_instances)
+            all_data = np.array(train_instances)
 
-    def classify(self, instance):
+            for mini_batch in generate_batches(all_data, BATCH_SIZE):
+                gradient = self.evaluate_gradient(mini_batch, weights)
+                weights += LEARNING_RATE * gradient
+
+                iteration += 1
+                if iteration % 10 == 0:
+                    test_accuracy = self.get_accuracy(dev_instances, weights)
+                    # if test_accuracy < last_accuracy:
+                    #     break
+                    # else:
+                    #     last_accuracy = test_accuracy
+                    self.model.update_weights(weights)
+            else:
+                continue
+            break
+
+    def classify(self, instance, weights=None, return_distribution=False):
         fnames = instance.features()
+        fnames.append(BIAS_FEATURE)
+
         # sparse representation is a list of feature id's
         # feature_id = feature_raw in weights_matrix
         sparse_vector = self.get_sparse_vector(fnames, self.model.map_name_to_id)
         prob_dict = {}
         for label in self.model.map_label_to_col:
-            prob = self.get_prob_given_X(sparse_vector, label)
+            prob = self.get_prob_given_X(label, sparse_vector, weights)
             prob_dict[label] = prob
 
-        return max(prob_dict, key=prob_dict.get)
+        if return_distribution:
+            return prob_dict
+        else:
+            return max(prob_dict, key=prob_dict.get)
 
     def get_sparse_vector(self, fnames, map_name_to_id):
         # todo what if feature name doesn't appear in mapping
+        # consider adding BIAS_FEATURE here instead.
+        # what if there are same fnames?
         return [map_name_to_id[name] for name in fnames]
 
-    def get_prob_given_X(self, sparse_vector, label):
+    def get_prob_given_X(self, label, sparse_vector, weights=None):
+        """
+            Given a sparse_vector that corresponds to X
+            calculates posterior probability P(Y=label | X)
+
+            Using current model.weights_matrix as default.
+            To avoid underflow while dividing 2 relatively close big numbers,
+            I will calculate it in log space and take exponent in the end.
+            Also using scipy.logsumexp function which is computationally stable
+        """
         col = self.model.map_label_to_col[label]
-        # todo exponents and normalization here?
-        return self.model.weights_matrix[sparse_vector, col].sum()
+        if weights is None:
+            weights = self.model.weights_matrix
+        # from weights_matrix get only the rows that correspond to the sparse_vector
+        relevant_weights = weights[sparse_vector, :]
+        weight_sums = relevant_weights.sum(axis=0)
+        tmp = weight_sums[col] - scp.logsumexp(weight_sums)
+        return np.exp(tmp)
 
     def calc_empirical_count(self, train_instances):
         observed_counts = np.zeros((self.model.rows, self.model.cols))
@@ -148,7 +203,7 @@ class MaxEnt(Classifier):
 
         return observed_counts
 
-    def calc_expected_count(self, train_instances):
+    def calc_expected_count(self, train_instances, weights):
         """
             Count the probabilities of current model
             1. For each document
@@ -156,15 +211,35 @@ class MaxEnt(Classifier):
                 compute the probability P(y=class | document)
                 and add to output matrix
 
+        :param weights: weights matrix to be used
         :param train_instances: type: list(Documents)
         :return: type: numpy.array(rows, columns)
-                 with same shape like model.observed_count
         """
         expected_counts = np.zeros((self.model.rows, self.model.cols))
         for instance in train_instances:
             sparse_vector = instance.feature_vector
             for label, col in self.model.map_label_to_col.items():
-                prob = self.get_prob_given_X(sparse_vector, label)
+                prob = self.get_prob_given_X(label, sparse_vector, weights)
                 expected_counts[sparse_vector, col] += prob
 
         return expected_counts
+
+    def get_accuracy(self, dev_instances, weights):
+        count = 0
+        for doc in dev_instances:
+            if self.classify(doc, weights) == doc.label:
+                count += 1
+        return count / len(dev_instances)
+
+    def evaluate_gradient(self, mini_batch, weights):
+        observed_count = self.calc_empirical_count(mini_batch)
+        expected_count = self.calc_expected_count(mini_batch, weights)
+        return observed_count - expected_count
+
+    def get_model(self):
+        return self._model
+
+    def set_model(self, new_model):
+        self._model = new_model
+
+    model = property(get_model, set_model)
--- src/test_maxent.py
+++ src/test_maxent.py
@@ -7,6 +7,7 @@ import sys
 class BagOfWords(Document):
     def features(self):
         """Trivially tokenize words."""
+
         return self.data.split()
 
 
@@ -22,6 +23,54 @@ def accuracy(classifier, test, verbose=sys.stderr):
         print >> verbose, "%.2d%% " % (100 * sum(correct) / len(correct)),
     return float(sum(correct)) / len(correct)
 
+
+def other_metrics(classifier, test, verbose=sys.stdout):
+    m_test = [x for x in test if x.label == "male"]
+    f_test = [x for x in test if x.label == "female"]
+    m_correct = len([x for x in m_test if classifier.classify(x) == x.label])
+    f_correct = len([x for x in f_test if classifier.classify(x) == x.label])
+    m_incorrect = len(m_test) - m_correct
+    f_incorrect = len(f_test) - f_correct
+    total_len = len(m_test) + len(f_test)
+    print("Test length: %d. \tMale/Female: %d/%d" % (total_len, len(m_test), len(f_test)))
+    print("Male   correct/incorrect %d/%d" % (m_correct, m_incorrect))
+    print("Female correct/incorrect %d/%d" % (f_correct, f_incorrect))
+
+    accuracy_percent = 100 * (m_correct + f_correct) / (len(m_test) + len(f_test))
+    classified_as_m = m_correct + f_incorrect
+    if classified_as_m == 0:
+        m_precision = 0
+    else:
+        m_precision = 100 * m_correct / classified_as_m
+    m_recall = 100 * m_correct / len(m_test)
+
+    classified_as_f = f_correct + m_incorrect
+    if classified_as_f == 0:
+        f_precision = 0
+    else:
+        f_precision = 100 * f_correct / classified_as_f
+    f_recall = 100 * f_correct / len(f_test)
+
+    if m_precision + m_recall == 0:
+        f_m_measure = 0
+    else:
+        f_m_measure = (2 * m_precision * m_recall) / (m_precision + m_recall)
+
+    if f_precision + f_recall == 0:
+        f_f_measure = 0
+    else:
+        f_f_measure = (2 * f_precision * f_recall) / (f_precision + f_recall)
+    if verbose:
+        print("Accuracy:\t %6.2f%%" % accuracy_percent)
+        print("------MALE------")
+        print("Precision:\t %6.2f%% " % m_precision)
+        print("Recall:\t\t %6.2f%% " % m_recall)
+        print("F_Measure:\t %6.2f%% " % f_m_measure)
+        print("-----FEMALE-----")
+        print("Precision:\t %6.2f%% " % f_precision)
+        print("Recall:\t\t %6.2f%% " % f_recall)
+        print("F_Measure:\t %6.2f%% " % f_f_measure)
+    return accuracy_percent / 100
     # class MaxEntTest(TestCase):
     #     u"""Tests for the MaxEnt classifier."""
     #
@@ -68,19 +117,41 @@ def split_names_corpus(document_class=Name):
     print "Total number of names: ", total_len
     seed(hash("names"))
     shuffle(names)
-    return names[:3], names[2:], names[2:]
-    # return names[:5000], names[5000:6000], names[6000:]
+    return names[:5000], names[5000:6000], names[6000:]
 
 
 def test_names_nltk():
     """Classify names using NLTK features.
        Document_class == Name"""
     train, dev, test = split_names_corpus()
+
+    # train = [BagOfWords("a1 b1 c1", 'Y'),
+    #          BagOfWords("a1 b1 c1", 'X'),
+    #          BagOfWords("a1 b1 c0", 'Y'),
+    #          BagOfWords("a0 b1 c1", 'X'),
+    #          BagOfWords("a0 b1 c1", 'Y'),
+    #          BagOfWords("a0 b0 c1", 'Y'),
+    #          BagOfWords("a0 b1 c0", 'X'),
+    #          BagOfWords("a0 b0 c0", 'X'),
+    #          BagOfWords("a0 b1 c1", 'Y')]
+    #
+    # test = [BagOfWords("a1 b0 c1"),
+    #         BagOfWords("a1 b0 c0"),
+    #         BagOfWords("a0 b1 c1"),
+    #         BagOfWords("a0 b1 c0")]
+    #
+    # dev = [BagOfWords("a1 b0 c1", 'Y'),
+    #        BagOfWords("a1 b0 c0", 'Y'),
+    #        BagOfWords("a0 b1 c1", 'Y'),
+    #        BagOfWords("a0 b1 c0", 'X')]
+
     classifier = MaxEnt()
     classifier.train(train, dev)
-    acc = accuracy(classifier, test)
-    print acc
+    other_metrics(classifier, test)
+
+    # dictionaries = [classifier.classify(x, return_distribution=True) for x in test]
+    # for dict in dictionaries:
+    #     print dict
 
 
-test_names_nltk()
 test_names_nltk()
